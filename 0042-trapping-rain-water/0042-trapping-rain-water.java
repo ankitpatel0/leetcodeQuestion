6ea2ca00921dd97c0d7a9dc978ee28609c031c77
Ankit Patel <ankitpatel1531@gmail.com>
2026-06-04 15:43:23 +0530

    Time: 1 ms (58.86%), Space: 48.1 MB (35.71%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] height) {
+        
+        int size = height.length;
+        if(size == 0){
+            return 0;
+        }
+        int[] left_max = new int[size];
+        int[] right_max = new int[size];
+        left_max[0]=height[0];
+        right_max[size-1]=height[size-1];
+        int result = 0;
+        for(int i = 1; i<size; i++){
+            left_max[i] = Math.max(height[i], left_max[i-1]);
+        }
+        for(int i = size-2; i>=0; i--){
+            right_max[i] = Math.max(height[i], right_max[i+1]);
+        }
+        for(int i = 0; i<size; i++){
+            int area = Math.min(left_max[i], right_max[i])-height[i];
+            result += Math.min(left_max[i], right_max[i])-height[i];
+
+        }
+        return result; 
+    }
+}
